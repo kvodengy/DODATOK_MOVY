f8a8249d6ef656e3c050fbea88ac26c5864c367a
kvodengy <kvodengy@gmail.com>
2025-04-11 18:22:28 +0300

    the test end left

--- practice.js
+++ practice.js
@@ -1,32 +1,59 @@
 let question = document.querySelector(".question")
 let options = document.querySelectorAll(".options")
 
+
 class Question {
     constructor(){
-        this.questions = [["1", "2", "3"],["4", "5", "6"],["7", "8", "9"]]
-        this.correct_answer
-        for(let i = 0; i < this.questions.length; i++){
-            this.question = this.questions[i][0]
-            this.correct_answer = this.questions[i][2]
-            while(this.randint(0,this.questions.length-1) == i){
-                this.answers = [
-                    correct_answer,
-                    this.questions[this.randint(0,this.questions.length-1)][2],
-                    this.questions[this.randint(0,this.questions.length-1)][2],
-                ]
-            
-            }
-
-            
-
-            /*if (this.randint(0,this.questions.length-1) == i){
-                this.answers.push(this.questions[this.randint(0,this.questions.length-1)[2]])
-                this.answers.push(this.questions[this.randint(0,this.questions.length-1)[2]])
-            }*/
-        console.log(this.answers)
-        this.shuffle(this.answers)
-        }
+        this.questions = [
+            ["coffe", "coffe", "кава"],
+            ["tea", "ti", "чай"],
+            ["croissant", "kwasɒŋ", "круасан"],
+            ["education", "ˌɛʤʊˈkeɪʃᵊn", "освіта"],
+            ["snake", "sneɪk", "змія"],
+            ["frame", "freɪm", "рамка"],
+            ["inflatable ball", "ɪnˈfleɪtəbᵊl bɔl", "надувна кулька"],
+            ["ceiling", "ˈsiːlɪŋ", "стеля"],
+            ["battery", "ˈbætᵊri", "батарея"],
+            ["pencil", "ˈpɛnsᵊl", "олівець"],
+            ["glass", "ɡlɑːs", "стакан"],
+            ["flower", "flaʊə", "квітка"],
+            ["plaster", "ˈplɑːstə", "штукатурка"],
+            ["brush", "brʌʃ", "пензель"],
+            ["bucket", "ˈbʌkɪt", "відро"]
+        ]
+    this.i = 0 
+    this.answers=[]
+    this.new_question()
     }
+
+    new_question(){
+        this.question = this.questions[this.i][0]
+        this.correct_answer = this.questions[this.i][2]
+        this.answers = [
+            this.correct_answer,
+            this.questions[this.randint(0,this.questions.length-1)][2],
+            this.questions[this.randint(0,this.questions.length-1)][2],
+        ]
+        while(this.randint(0,this.questions.length-1) == this.i){
+            this.answers = [
+                this.correct_answer,
+                this.questions[this.randint(0,this.questions.length-1)][2],
+                this.questions[this.randint(0,this.questions.length-1)][2],
+            ]
+        }
+        this.shuffle(this.answers)
+        console.log(this.answers)
+    }
+    check_question (){
+        if(this.i < this.questions.length-1){
+            this.new_question()
+            this.i += 1
+            return true
+        }
+        else{
+            return false
+        }
+        }
     display(){
         question.innerHTML = this.question
         for(let i = 0; i < this.answers.length; i++){
@@ -58,20 +85,28 @@ for(let i = 0; i < options.length; i++){
             correct_answers_given += 1
             anime({
                 targets: options[i],
-                backgroundColor: ["rgb(0, 255, 0)" ,"#f73868"],
+                backgroundColor: ["rgb(0, 255, 0)" ,"#EEFF00"],
                 easing: 'easeOutQuad',
                 duration:600
             })
         }else{
             anime({
                 targets: options[i],
-                backgroundColor: ["rgb(255, 0, 0)" ,"#f73868"],
+                backgroundColor: ["rgb(255, 0, 0)" ,"#EEFF00"],
                 easing: 'easeOutQuad',
                 duration:600
             })
         }
-        question_counter += 1
-        current_question = new Question
-        current_question.display()
+        if (current_question.check_question() == true){
+            question_counter += 1
+            current_question.new_question()
+            current_question.display()
+        }
+        else{
+            question.innerHTML = "Тест пройдено"
+            for(let i; i<options.length; i++){
+                options[i].style.display = "none"
+            }
+        }
     })
 }
